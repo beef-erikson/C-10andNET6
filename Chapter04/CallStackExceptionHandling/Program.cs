670dd198d5cac39ed83f4e731a4be05c11a54571
Troy Martin <beef.erikson.studios@gmail.com>
2022-09-21 13:22:23 -0400

    Chapter 4 end - pre-exercises

---
+++ Chapter04/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Packt;
+using static System.Console;
+
+WriteLine("In main");
+Alpha();
+
+static void Alpha()
+{
+    WriteLine("In alpha");
+    Beta();
+}
+
+static void Beta()
+{
+    WriteLine("In beta");
+
+    try
+    {
+        Calculator.Gamma();
+    }
+    catch (Exception ex)
+    {
+        WriteLine($"Caught this: {ex.Message}");
+        throw;
+    }
+}
---
+++ Chapter04/CallStackExceptionHandlingLib/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace Packt
+{
+    public class Calculator
+    {
+        public static void Gamma()  // public so it can be called from outside
+        {
+            WriteLine("In gamma");
+            Delta();
+        }
+
+        private static void Delta()  // private so it can only be called internally
+        {
+            WriteLine("In delta");
+            File.OpenText("bad file path");
+        }
+    }
+}
