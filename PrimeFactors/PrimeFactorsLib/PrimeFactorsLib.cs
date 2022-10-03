58b25705bd99280f32794c132a3d6f7edaf4e935
Troy Martin <beef.erikson.studios@gmail.com>
2022-10-02 23:30:08 -0400

    Finished exercise.

--- PrimeFactors/PrimeFactorsLib/PrimeFactorsLib.cs
+++ PrimeFactors/PrimeFactorsLib/PrimeFactorsLib.cs
@@ -11,8 +11,6 @@ public class PrimeFactorsLib
     /// <returns>String of all prime factors.</returns>
     public string PrimeFactors(int number)
     {
-
-
         // Divide number by smallest prime number
         for (int i = 2; i <= 1_000; i++)
         {
@@ -22,23 +20,13 @@ public class PrimeFactorsLib
             }
             else if (number != 1)
             {
-                Console.WriteLine($"Adding factor: {i}");
                 factors += $"{i} ";
                 number = number / i;
-                Console.WriteLine($"Using number {number}");
                 PrimeFactors(number);
             }
 
             return factors;
         }
-
-
-
-        // Divide quotient by smallest prime number
-
-        // Repeat until quotient becomes one
-
-        // Multiply all prime factors should equal the same number
 
         return factors;
     }
