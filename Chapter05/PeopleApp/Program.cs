818da248a8c8cd39577d74dec545b95909567f0c
Troy Martin <beef.erikson.studios@gmail.com>
2022-10-03 02:02:44 -0400

    Fields initialized with new Person instatiations.

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -3,5 +3,22 @@
 using Packt.Shared;
 using static System.Console;
 
+// Makes a new person, beef
 Person beef = new();
-WriteLine(beef.ToString());
+beef.Name = "Beef";
+beef.DateOfBirth = new(1981, 6, 2);
+
+WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}.",
+    arg0: beef.Name,
+    arg1: beef.DateOfBirth);
+
+// Makes another person, boot using an object initializer (C# 9+)
+Person boot = new()
+{
+    Name = "Boot",
+    DateOfBirth = new(2023, 9, 15)
+};
+
+WriteLine(format: "{0} was born on {1:dd MMM yy}.",
+    arg0: boot.Name,
+    arg1: boot.DateOfBirth);
